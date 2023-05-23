157528331c44326bb4a42f7b83a2183bd909a8f1
Maksym <90103151+MaksVibe@users.noreply.github.com>
2023-05-23 22:02:10 +0300

    Update App and styles: common, vars, index, App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import Header from "./components/Header/Header";
+import Header from "./sections/Header/Header";
+import Hero from "./sections/Hero/Hero";
+import "./App.scss";
+import Users from "./sections/Users/Users";
 
 function App() {
   return (
     <>
       <Header />
+      <div className="container">
+        <main className="main">
+          <Hero />
+          <Users />
+        </main>
+      </div>
     </>
   );
 }
