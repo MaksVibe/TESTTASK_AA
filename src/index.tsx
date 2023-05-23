b46d19f5c276ba783c21fb3c024c2d1ad585f40d
Maksym <90103151+MaksVibe@users.noreply.github.com>
2023-05-23 21:58:26 +0300

    Add provider with store to use redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-import "./fonts/Nunito-Regular.ttf";
+import "./assets/fonts/Nunito-Regular.ttf";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
