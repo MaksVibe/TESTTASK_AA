aeecb8e9ca0b8b8390a07083f60722b1b0edcc9a
Maksym <90103151+MaksVibe@users.noreply.github.com>
2023-05-25 12:51:18 +0300

    Implement Loader into Users section

--- src/sections/Users/Users.tsx
+++ src/sections/Users/Users.tsx
@@ -1,24 +1,28 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "../../components/Button/Button";
+import Loader from "../../components/Loader/Loader";
 import Title from "../../components/Title/Title";
 import User from "../../components/User/User";
 import { fetchUsers } from "../../redux/users/usersOperations";
-import { getUsers } from "../../redux/users/usersSelector";
+import { getUsers, getIsLoading } from "../../redux/users/usersSelector";
 import "./Users.scss";
 
 const Users: React.FC = () => {
   const dispatch = useDispatch();
   const users = useSelector(getUsers);
+  const isLoading = useSelector(getIsLoading);
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
   return (
-    <div className="users__wrapper">
+    <div className="users__wrapper" id="users">
       <Title users />
-      {users && (
+      {users.length < 1 ? (
+        <Loader />
+      ) : (
         <>
           <ul className="users__list">
             {users.map((user: any) => (
@@ -27,6 +31,7 @@ const Users: React.FC = () => {
               </li>
             ))}
           </ul>
+          {isLoading && <Loader />}
           <Button showMore />
         </>
       )}
